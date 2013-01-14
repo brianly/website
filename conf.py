adf3011aaee33f528616dd44c4f3d9a29ed36dc2
Brian Lyttle <github@killaloo.org>
2013-01-13 22:14:46 -0500

    Cleanup config

--- conf.py
+++ conf.py
@@ -10,13 +10,16 @@ EMAIL = 'blog@brianlyttle.com'
 
 FILTERS = ['markdown+codehilite(css_class=highlight)', 'hyphenate', 'h1']
 VIEWS = {
-    '/': {'filters': 'summarize', 'view': 'index',
-          'pagination': '/page/:num'},
+    '/': {'filters': 'summarize',
+          'view': 'index',
+          'pagination': '/page/:num',
+          'items_per_page': 5},
 
     '/:year/:month/:slug/': {'view': 'entry',
                     'template': 'archive.html'},
 
-    '/tag/:name/': {'filters': 'summarize', 'view':'tag',
+    '/tag/:name/': {'filters': 'summarize',
+                    'view':'tag',
                     'pagination': '/tag/:name/:num'},
 
     # per tag Atom or RSS feed. Just uncomment to generate them.
@@ -46,8 +49,8 @@ VIEWS = {
     # '/rss/python/': {'filters': 'h2', 'view': 'rss',
     #                  'if': lambda e: 'python' in e.tags}
 
-    # # a full typography features entry including MathML and Footnotes
-    # '/:year/:slug': {'filters': ['typography', 'Markdown+Footnotes+MathML'],
+    # a full typography features entry including MathML and Footnotes
+    # '/:year/:slug': {'filters': ['typography', 'Markdown'],
     #                  'view': 'entry'}
 }
 
